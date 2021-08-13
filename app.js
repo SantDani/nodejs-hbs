9696819de0bcc056b807d6061b9698829fc02af1
SantiDani <profsantiRP@gmail.com>
2021-08-13 17:27:55 +0200

    All pages

--- app.js
+++ app.js
@@ -19,4 +19,53 @@ app.get('/', (req, res) => {
     res.render('index' , {title: 'Home Page HBS'});
 });
 
+app.get('/films', (request,  response) => {
+    response.render('films', {
+        title: 'films',
+        films: [
+            {
+                id: 1,
+                name: 'Jhon Wick',
+                generes: ['action', 'crime', 'thriller']
+            },
+            {
+                id: 2,
+                name: 'Fast and Furious 9',
+                generes: ['action', 'adventure', 'crime']
+            }
+        ]
+    });
+});
+
+app.get('/generes', (request, response)=>{
+    response.render('generes', {
+        title: 'generes',
+        generes:[
+            {
+                status: false,
+                name: 'action'
+            }
+            ,{
+                status: false,
+                name: 'adventure'
+            }
+            ,{
+                status: false,
+                name: 'crime'
+            },
+            {
+                status: false,
+                name: 'thriller'
+            },
+        ]
+    })
+});
+
+app.use((request, response) => {
+    response.status(404).render('404', {
+        title: 404,
+        description: 'Page not found'
+    });
+});
+
 app.listen(PORT, ()=> console.log(`Express is listen at http://localhost:${PORT}`));
